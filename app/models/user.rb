fb064d2a5b9d1d94f652209be916a6bc43b3393b
axolotlfish <canlascyrilchristian@gmail.com>
2023-06-08 16:10:46 +0800

    init

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+    class User < ApplicationRecord
+        has_many :posts
+        has_many :comments
+
+        validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
+        validates :email, presence: true, uniqueness: true
+        validates :password, presence: true, length: { in: 6..16 }
+        
+    end
